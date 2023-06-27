5b093c396fdc8e3368025894a9ca77f2aae1ce42
Splix <federicosalalois@gmail.com>
2023-06-27 14:30:22 +0200

    update

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -1,13 +1,36 @@
 #include "../include/minishell.h"
 
+void    do_export(t_minishell *minishell, int i)
+{
+    char    **a;
+    char    *name;
+    char    *val;
+    t_env   *env;
+
+    a = minishell->command->cmd_args;
+    env = minishell->env;
+    name = ft_substr(a[i], 0, ft_strchr(a[i], '=') - a[i]);
+    val = ft_substr(a[i], ft_strchr(a[i], '=') - a[i] + 1, ft_strlen(a[i]));
+    if (val[0] == '\0' && a[i + 1] != NULL)
+    {
+        free(val);
+        val = ft_strdup(a[i + 1]);
+    }
+    if (find_env_var_node(minishell, name) == NULL)
+        ft_add_env_node(&minishell->env, name, val);
+    else
+    {
+        env = find_env_var_node(minishell, name);
+        free(env->value);
+        free(name);
+        env->value = val;
+    }
+}
+
 void    ft_export(t_minishell *minishell)
 {
-    t_env   *env;
-    char    *name;
-    char    *value;
     int     i;
 
-    env = minishell->env;
     if (minishell->command->cmd_args == NULL)
     {
         ft_putstr_fd("export: not enough arguments\n", 2);
@@ -18,24 +41,7 @@ void    ft_export(t_minishell *minishell)
     while (minishell->command->cmd_args[i])
     {
         if (ft_strchr(minishell->command->cmd_args[i], '='))
-        {
-            name = ft_substr(minishell->command->cmd_args[i], 0, ft_strchr(minishell->command->cmd_args[i], '=') - minishell->command->cmd_args[i]);
-            value = ft_substr(minishell->command->cmd_args[i], ft_strchr(minishell->command->cmd_args[i], '=') - minishell->command->cmd_args[i] + 1, ft_strlen(minishell->command->cmd_args[i]));
-            if (value[0] == '\0' && minishell->command->cmd_args[i + 1] != NULL)
-            {
-                value = ft_strdup(minishell->command->cmd_args[i + 1]);
-                i++;
-            }
-            if (find_env_var_node(minishell, name) == NULL)
-                ft_add_env_node(&minishell->env, name, value);
-            else
-            {
-                env = find_env_var_node(minishell, name);
-                free(env->value);
-                free(name);
-                env->value = value;
-            }
-        }
+            do_export(minishell, i);
         i++;
     }
 }
